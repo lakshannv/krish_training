3759954eeb13224c62973d272835b7642c6764bf
Lakshan <lakshannv@gmail.com>
2022-02-27 17:28:05 +0530

    Initial Commit

---
+++ Assignment 01/a/A.java
@@ -0,0 +1,8 @@
+package a;
+
+public class A {
+
+	public void run() {
+		System.out.println("A is Running");
+	}
+}
---
+++ Assignment 01/b/B.java
@@ -0,0 +1,11 @@
+package b;
+
+import a.A;
+
+public class B extends A {
+
+	@Override
+	public void run() {
+		System.out.println("B is Running");
+	}
+}
